8a81e70137aa8cc77661e8d7577c4d4c5c131823
kashif-hussain6 <kashifdayal4l@gmail.com>
2025-05-08 19:25:23 +0500

    fixed the  issue here

--- components/ui/radio-group.jsx
+++ components/ui/radio-group.jsx
@@ -5,7 +5,6 @@ import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { Circle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-
 const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
   return (<RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />);
 })
@@ -24,6 +23,7 @@ const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
         <Circle className="h-3.5 w-3.5 fill-primary" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>)
+    
   );
 })
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
